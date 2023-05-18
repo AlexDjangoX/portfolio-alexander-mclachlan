823c61e675898c417aac2869b2757318afa8fbde
AlexDjangoX <alexmonk17@gmail.com>
2023-05-18 15:29:10 +0200

    Added opengraph-image.png

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ const MyApp = ({ Component, pageProps }) => (
       <title>Alexander McLachlan</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
-      <link rel="preconnect" href="https://stijndv.com" />
     </Head>
     <Component {...pageProps} />
   </>
